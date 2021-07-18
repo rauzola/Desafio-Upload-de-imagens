f2826d7739cb7a38e3e437ae42175193efdf387a
rauzola <raul_sigoli@hotmail.com>
2021-07-18 17:32:43 -0300

    finish challenge

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,5 @@
 import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -16,17 +17,33 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [selectedImage, setSelectedImage] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (url: string): void => {
+    setSelectedImage(url);
+    onOpen();
+  };
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="40px">
+        {cards &&
+          cards.map(card => (
+            <Card
+              key={card.id}
+              data={card}
+              viewImage={() => handleViewImage(card.url)}
+            />
+          ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImage}
+      />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
 import { Button, Box } from '@chakra-ui/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
+import Head from 'next/head';
 
-import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
-import { api } from '../services/api';
-import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { api } from '../services/api';
+
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+type IInfiniteQueryResponse = {
+  after: number | null;
+  data: Card[];
+};
 
 export default function Home(): JSX.Element {
+  const getImages = async ({
+    pageParam = null,
+  }): Promise<IInfiniteQueryResponse> => {
+    const { data } = await api.get('/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  };
+
   const {
     data,
     isLoading,
@@ -16,28 +42,39 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
+  } = useInfiniteQuery<unknown, unknown, IInfiniteQueryResponse>(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    getImages,
+    {
+      getNextPageParam: (lastPage: { after: number }) => lastPage.after,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const imgsData = data?.pages.map(page => page.data).flat();
+    return imgsData;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
+      <Head>
+        <title>UpFi</title>
+        <link rel="shortcut icon" href="logo.svg" />
+      </Head>
+
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button mt={8} onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
